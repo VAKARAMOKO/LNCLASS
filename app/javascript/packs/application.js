d7d5cbcecba2d723e266b5b898c881b308bdcd9e
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-09-30 10:44:33 +0000

    install font icon CDN

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,10 @@ require("jquery3")
 require("popper")
 require("bootstrap")
 
+require("bootstrap-datepicker/core")
+require("bootstrap-datepicker/locales/bootstrap-datepicker.es.js")
+require("bootstrap-datepicker/locales/bootstrap-datepicker.fr.js")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -19,3 +23,4 @@ require("bootstrap")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import "../css/application.css"
+import "../../assets/stylesheets/application.scss"
